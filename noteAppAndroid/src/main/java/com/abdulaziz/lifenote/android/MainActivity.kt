d399078ed00f8c1fe881031c788c9b2c1e35d612
SYSTEMS\abdulaziz.rafiq <abdulaziz.rafiq@systemsltd.com>
2022-11-30 17:34:21 +0500

    Main Activity CHanges

--- noteAppAndroid/src/main/java/com/abdulaziz/lifenote/android/MainActivity.kt
+++ noteAppAndroid/src/main/java/com/abdulaziz/lifenote/android/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.abdulaziz.lifenote.android
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -16,7 +24,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.abdulaziz.lifenote.Greeting
 
 @Composable
 fun MyApplicationTheme(
@@ -61,27 +68,72 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
+            MyApplicationTheme(darkTheme = false) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting(Greeting().greeting())
+                    Column() {
+                        TitleBar("Hello, Aziz!")
+                    }
                 }
             }
         }
     }
 }
+@Composable
+fun centerItems(){
+
+}
 
 @Composable
-fun Greeting(text: String) {
-    Text(text = text)
+fun TitleBar(text: String) {
+
+    var stringText = remember {
+        mutableStateOf("")
+    }
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .height(240.dp)
+        .clip(RoundedCornerShape(bottomEnd = 16.dp, bottomStart = 16.dp))
+        .background(color = MaterialTheme.colors.primary)
+        .padding(16.dp),
+    verticalArrangement = Arrangement.Bottom,
+    horizontalAlignment = Alignment.Start) {
+        Text(
+            text = text,
+            fontSize = 34.sp,
+            color=Color.White,
+            fontWeight = FontWeight.ExtraBold,
+            modifier = Modifier
+                .fillMaxWidth())
+        Text(
+            text = "What's on your mind!",
+            fontSize = 16.sp,
+            color=Color.White.copy(alpha = 0.9f),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth())
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(modifier = Modifier
+            .fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp),
+            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent),
+            label = {Text(text = "Write here ...")},
+            value = stringText.value, onValueChange = {
+                stringText.value = it
+        })
+        Spacer(modifier = Modifier.height(8.dp))
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        Greeting("Hello, Android!")
+        TitleBar("Hello, Aziz!")
     }
 }
